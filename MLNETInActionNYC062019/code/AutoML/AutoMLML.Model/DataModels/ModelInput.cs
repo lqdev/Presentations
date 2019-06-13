4079156718330deb8311ece1c5fd356a1fe48002
Luis Quintanilla <lqdev@outlook.com>
2019-06-12 20:40:26 -0400

    AutoML Sample

---
+++ MLNETInActionNYC062019/code/AutoML/AutoMLML.Model/DataModels/ModelInput.cs
@@ -0,0 +1,22 @@
+//*****************************************************************************************
+//*                                                                                       *
+//* This is an auto-generated file by Microsoft ML.NET CLI (Command-Line Interface) tool. *
+//*                                                                                       *
+//*****************************************************************************************
+
+using Microsoft.ML.Data;
+
+namespace AutoMLML.Model.DataModels
+{
+    public class ModelInput
+    {
+        [ColumnName("SentimentText"), LoadColumn(0)]
+        public string SentimentText { get; set; }
+
+
+        [ColumnName("Sentiment"), LoadColumn(1)]
+        public bool Sentiment { get; set; }
+
+
+    }
+}
---
+++ MLNETInActionNYC062019/code/AutoML/SentimentAnalysisWebAPI/Controllers/PredictController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using AutoMLML.Model.DataModels;
+using Microsoft.Extensions.ML;
+
+namespace SentimentAnalysisWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PredictController : ControllerBase
+    {
+        private readonly PredictionEnginePool<ModelInput, ModelOutput> _predictionEnginePool;
+
+        public PredictController(PredictionEnginePool<ModelInput, ModelOutput> predictionEnginePool)
+        {
+            _predictionEnginePool = predictionEnginePool;
+        }
+
+        [HttpPost]
+        public ActionResult<string> Post([FromBody] ModelInput input)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            ModelOutput prediction = _predictionEnginePool.Predict(input);
+
+            string sentiment = Convert.ToBoolean(prediction.Prediction) ? "Positive" : "Negative";
+
+            return Ok(sentiment);
+        }
+    }
+}
