1d06116cbfb38c13945ffc721470e60e151e2b7e
LQ <lqdev@outlook.com>
2019-10-12 11:25:29 -0400

    Added streaming sample

---
+++ codecampnyc2019/code/SparkNETStreaming/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.Spark.Sql; 
+using Microsoft.Spark.Sql.Streaming;
+using static Microsoft.Spark.Sql.Functions;
+
+namespace SparkNETStreaming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SparkSession ss = 
+                SparkSession
+                    .Builder()
+                    .AppName(".NET for Spark Streaming")
+                    .GetOrCreate();
+            
+            DataFrame stream =
+                ss
+                .ReadStream()
+                .Format("socket")
+                .Option("host","localhost")
+                .Option("port",9000)
+                .Load();
+
+            DataFrame grade = 
+                stream
+                    .Select(Col("value"));
+
+            StreamingQuery query = 
+                grade
+                    .WriteStream()
+                    .OutputMode(OutputMode.Append)
+                    .Format("console")
+                    .Start();
+
+            query.AwaitTermination();
+        }
+    }
+}
