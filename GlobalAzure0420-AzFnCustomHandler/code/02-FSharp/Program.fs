eb8b353dc41f6fc92da103306e2bf1f8a67f427c
luisquintanilla <Luis.Quintanilla@microsoft.com>
2020-04-24 01:13:00 -0400

    Adding custom handler files

---
+++ GlobalAzure0420-AzFnCustomHandler/code/02-FSharp/Program.fs
@@ -0,0 +1,21 @@
+﻿open System
+open Saturn
+open Giraffe
+
+// Define PORT
+let PORT = Environment.GetEnvironmentVariable("FUNCTIONS_HTTPWORKER_PORT")
+let URL = sprintf "http://0.0.0.0:%s" PORT
+
+// Define route handlers
+let getValues = {|value=[|1;2;3|]|}
+
+let apiRouter = router {
+    get "/values" (getValues |> json)
+}
+
+let app = application {
+    use_router apiRouter
+    url URL
+}
+
+run app
